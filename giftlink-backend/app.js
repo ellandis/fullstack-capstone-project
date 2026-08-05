9cc88590b184aec31065996b67d0fd3f186b3b73
ellandis <elamarlandis@gmail.com>
2026-08-05 15:21:10 -0400

    updated app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -27,7 +27,6 @@ const giftRoutes = require('./routes/giftRoutes');
 
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-app.use('/api/gifts', giftRoutes);
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -36,7 +35,7 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const logger = require('../logger');
+
 
 const connectToDatabase = require('../models/db');
 
