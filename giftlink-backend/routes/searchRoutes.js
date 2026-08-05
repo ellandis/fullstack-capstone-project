df2fffc4f7c39d4ef6a04abdfbf9e8fbe5feca00
ellandis <elamarlandis@gmail.com>
2026-08-05 14:56:28 -0400

    Api gift service

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -6,7 +6,7 @@ const connectToDatabase = require('../models/db');
 router.get('/', async (req, res, next) => {
     try {
         // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
+        const db = await connectToDatabase();
 
         const collection = db.collection("gifts");
 
