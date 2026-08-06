bd1a774480751b652d71a41f4d0e2c20df3369a0
ellandis <elamarlandis@gmail.com>
2026-08-06 08:40:12 -0400

    npm sentiment fix

--- sentiment/index.js
+++ sentiment/index.js
@@ -7,7 +7,7 @@ const expressPino = require('express-pino-logger')({ logger });
 const natural = require("natural");
 
 // Task 2: initialize the express server
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -52,7 +52,8 @@ app.post('/sentiment', async (req, res) => {
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        
+        res.status(500).json({ message: 'Error performing sentiment analysis' });
+
     }
 });
 
