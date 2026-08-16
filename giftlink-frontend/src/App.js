d4dcf169f37d4c7af8e6e920a5f74740084b5f0f
ellandis <elamarlandis@gmail.com>
2026-08-16 18:19:48 -0400

    Lint update

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -9,7 +9,7 @@ import Profile from './components/Profile/Profile';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
   return (
 		<>
         <Navbar/>
